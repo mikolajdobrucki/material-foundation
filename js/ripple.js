2543e1d3cd09084d7a159c75abaa6efbd27e813f
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2017-10-04 10:45:43 +0200

    new ripple effect
    credits go to: Craig Tuttle
    https://codepen.io/Craigtut/pen/dIfzv

--- js/ripple.js
+++ js/ripple.js
@@ -1,25 +1,40 @@
-var addRippleEffect = function (e) {
-    var target = e.target;
-    if (target.className.indexOf("ink") === -1) { return false; }
-    var rect = target.getBoundingClientRect();
-    var ripple = target.querySelector('.ripple');
-    if (!ripple) {
-        ripple = document.createElement('span');
-        if (target.className.indexOf("ink-color") !== -1) {
-            ripple.className = 'ripple-color';
-        } else {
-            ripple.className = 'ripple';
-        }
-        ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
-        target.appendChild(ripple);
-    }
-    ripple.classList.remove('show');
-    var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
-    var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
-    ripple.style.top = top + 'px';
-    ripple.style.left = left + 'px';
-    ripple.classList.add('show');
-    return false;
-};
+// Ripple Effect
+// by Craig Tuttle
+// http://codepen.io/Craigtut/pen/dIfzv
 
-document.addEventListener('click', addRippleEffect, false);
+(function (window, $) {
+
+  $(function() {
+
+
+    $('.ripple, .button, .tabs-title').on('click', function (event) {
+      event.preventDefault();
+
+      var $div = $('<div/>'),
+          btnOffset = $(this).offset(),
+      		xPos = event.pageX - btnOffset.left,
+      		yPos = event.pageY - btnOffset.top;
+
+
+
+      $div.addClass('ripple-effect');
+      var $ripple = $(".ripple-effect");
+
+      $ripple.css("height", $(this).height());
+      $ripple.css("width", $(this).height());
+      $div
+        .css({
+          top: yPos - ($ripple.height()/2),
+          left: xPos - ($ripple.width()/2),
+          background: $(this).data("ripple-color")
+        })
+        .appendTo($(this));
+
+      window.setTimeout(function(){
+        $div.remove();
+      }, 2000);
+    });
+
+  });
+
+})(window, jQuery);
